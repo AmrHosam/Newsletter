bc29516c0e8ec1ae7fab7c0242623d88f504cdab
Amr Hossam <amrhossam97@gmail.com>
2021-03-10 06:39:24 +0200

    Add success and failure pages

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ app.post("/", function(req, res){
     const request = https.request(url, options, function(response){
 
         if(response.statusCode === 200) {
-            res.send("Successfully subscribed!")
+            res.sendFile(__dirname + "/success.html")
         }
         else {
-            res.send("Ther was an error with signin up, please try again!");
+            res.sendFile(__dirname + "/failure.html");
         }
         response.on("data", function(data){
             console.log(JSON.parse(data));
@@ -56,4 +56,8 @@ app.post("/", function(req, res){
     });
     request.write(jsonData);
     request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
 });
